b63df7e4e7d16f651c68d76b24e6065af1f5dba2
tantrungse <tantrungse@gmail.com>
2023-08-14 10:14:30 +0700

    Signup

--- Core/models.py
+++ Core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
+# User = get_user_model()
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,4 +11,4 @@ class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
-        return self.user.get_username
+        return str(self.user.username)
