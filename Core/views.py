a94c23a44d52f84ced876e8f2efcd508a98838d9
tantrungse <tantrungse@gmail.com>
2023-08-18 18:08:03 +0700

    Like post

--- Core/views.py
+++ Core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 @login_required(login_url='signin')
 def index(request):
@@ -92,4 +92,22 @@ def upload(request):
         
         return redirect('/')
     else:
+        return redirect('/')
+    
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    post = Post.objects.get(id=post_id)
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes += 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes -= 1
+        post.save()
         return redirect('/')
