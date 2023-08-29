cee468015a742b6ed6fb8e4589f9a2bd3b2da70f
tantrungse <tantrungse@gmail.com>
2023-08-29 09:10:55 +0700

    Update requirements.txt anh settings

--- SocialBook/settings.py
+++ SocialBook/settings.py
@@ -150,11 +150,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-CSRF_TRUSTED_ORIGINS = ['https://web-production-3cf4c.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://web-production-3cf4c.up.railway.app']
 
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
